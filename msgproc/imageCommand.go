67416d466ed62cc01f2e39987bdcb9797fe43f9d
Kharriz Abiyasa Bhimabrata <kharizet@gmail.com>
2019-09-14 07:38:29 +0700

    fixed imageCommand parsing bug

--- msgproc/imageCommand.go
+++ msgproc/imageCommand.go
@@ -15,9 +15,13 @@ func (imageCommand ImageCommand) IsExecuted(update BotUpdate) bool {
 }
 
 func (imageCommand ImageCommand) Process(update BotUpdate) BotReply {
-	title, image, err := qwantservice.GetImage(strings.Split(update.Message(), "gbr ")[0])
+	title, image, err := qwantservice.GetImage(getCommandArguments(update.Message()))
 	if err != nil {
 		return NewBotMsgReply(update, "image not found")
 	}
 	return NewBotImgReply(update, image, title)
+}
+
+func getCommandArguments(input string) string {
+	return strings.Split(input, "gbr ")[1]
 }
